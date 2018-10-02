3dd29fda7e186ebfd189a7bf86f0e44a029758d5
Kelly Sheddy <jungaka07@gmail.com>
2018-10-02 18:11:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-nums = [1, 2, 3, 4, 5] 
-
-def my_select(collection) 
-  empty_arr = [] #can handle an empty collection
-  
-  i = 0 #counter!
-  while i < collection.length #while the collection has items
-  empty_arr.push yield(collection[i]) #push items into the new empty array,  YIELD means to shoot down to the statement below
-  i += 1 #keep doing this unil you meet the while statement
-end 
-
-  empty_arr.compact  #compact removes "nil"
-end 
-
-
-my_select(nums) do |num| #HERE is where YIELD goes to 
-  if num.even? #this code checks for even numbers within the collection 
-    num #if even this line hands it back to be added to the empty array 
-  end 
-end 
 
 nums = [1, 2, 3, 4, 5] 
 
